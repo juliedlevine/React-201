d602dd58fce5c6cc0c579560c39091af76327a7e
juliemdyer <juliemdyer@gmail.com>
2017-04-25 15:51:48 -0400

    Added styling and stalemate check / play again

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -1,4 +1,6 @@
 /*jshint esversion: 6 */
+// eslint-disable-next-line to ignore the next line.
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -41,17 +43,23 @@ class TicTacToe extends React.Component {
         let gameOver = false;
         let b = this.state.board;
         if (b[0] === 'X' && b[1] === 'X' && b[2] === 'X' || b[3] === 'X' && b[4] === 'X' && b[5] === 'X' || b[6] === 'X' && b[7] === 'X' && b[8] === 'X' || b[0] === 'X' && b[3] === 'X' && b[6] === 'X' || b[1] === 'X' && b[4] === 'X' && b[7] === 'X' || b[2] === 'X' && b[5] === 'X' && b[8] === 'X' || b[0] === 'X' && b[4] === 'X' && b[8] === 'X' || b[2] === 'X' && b[4] === 'X' && b[6] === 'X') {
-            message = 'X wins';
+            message = 'X wins!';
             gameOver = true;
 
         }
         if (b[0] === 'O' && b[1] === 'O' && b[2] === 'O' || b[3] === 'O' && b[4] === 'O' && b[5] === 'O' || b[6] === 'O' && b[7] === 'O' && b[8] === 'O' || b[0] === 'O' && b[3] === 'O' && b[6] === 'O' || b[1] === 'O' && b[4] === 'O' && b[7] === 'O' || b[2] === 'O' && b[5] === 'O' && b[8] === 'O' || b[0] === 'O' && b[4] === 'O' && b[8] === 'O' || b[2] === 'O' && b[4] === 'O' && b[6] === 'O') {
-            message = 'O wins';
+            message = 'O wins!';
+            gameOver = true;
+        }
+
+        if (!this.state.board.includes('') && gameOver === false) {
+            message = 'Stalemate!';
             gameOver = true;
         }
 
         return (
             <div className="main">
+                <h1>TIC TAC TOE</h1>
                 <div className={classnames('board', {'disabled' : gameOver===true})}>
                     {this.state.board.map((square, index) =>
                         <div key={index}
@@ -60,8 +68,10 @@ class TicTacToe extends React.Component {
                             {this.state.board[index]}
                         </div>
                     )}
-                    <div>{message}</div>
+                    <div className="clearfix"></div>
+                    <div className="message">{message}</div>
                 </div>
+
                 {gameOver ? <button onClick={() => this.playAgain()}>Play Again?</button> : <br/>}
             </div>
         )
